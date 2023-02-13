ab03dec7da994a06deeeae63b5e33652935e4cea
Sylvio <sylviomafort@gmail.com>
2023-02-13 15:24:06 +0900

    first commit

---
+++ app/contact/layout.tsx
@@ -0,0 +1,14 @@
+// import "./globals.css";
+
+export default function ContactLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div>
+      <div>Contact layout test</div>
+      <div>{children}</div>
+    </div>
+  );
+}
